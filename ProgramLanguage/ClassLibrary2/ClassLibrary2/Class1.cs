c53609518d27fb846684725680d7fffcbdb81561
freekode1ko <freekode1ko@gmail.com>
2019-03-10 15:30:54 +0300

    .dll for lab 2 and 3

---
+++ ProgramLanguage/ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    /// <summary>
+    /// Генерация случайных чисел для получения порядка вывода ответов
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// Метод генерации случайных чисел для получения порядка вывода ответов
+        /// </summary>
+        /// <param name="max"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static int[] Rand(int max, int[] x)
+        {       
+            Random r = new Random();
+            for (int i = 0; i < max; i++)
+            {
+                bool contains;
+                int next;
+                do
+                {
+                    next = r.Next(max);
+                    contains = false;
+                    for (int index = 0; index < i; index++)
+                    {
+                        int n = x[index];
+                        if (n == next)
+                        {
+                            contains = true;
+                            break;
+                        }
+                    }
+                } while (contains);
+                x[i] = next;
+            }
+            return x;
+        }
+    }
+}
---
+++ ProgramLanguage/ClassLibraryLab2/ClassLibraryLab2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryLab2
+{
+    /// <summary>
+    /// Решение системы уравнения
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// Метод решения системы уравнения
+        /// </summary>
+        /// <param name="B"></param>
+        /// <param name="C"></param>
+        /// <param name="X"></param>
+        /// <returns></returns>
+        public static double Ans(double B, double C, double X)
+        {
+            double LocalPEr;
+            if (X > 1)
+            { return X * (Math.Sqrt(B * B + C * C)); }
+            if (X < 0)
+            {
+                 LocalPEr = Math.Min(Math.Sqrt(B), X * X);
+                 return Math.Min(LocalPEr, X + C);
+                //LocalPEr = minn(Math.Sqrt(B), X * X);
+                // return minn(LocalPEr, X + C);
+            }
+            return Math.Max(Math.Log(B), X + C);
+            //return maxx(Math.Log(B), X + C);
+        }
+
+        //Методы для вычисления максимума и минимума
+        static double maxx(double x, double y)
+        {
+            if (x > y)
+                return x;
+            else
+                return y;
+        }
+        static double minn(double x, double y)
+        {
+            if (x < y)
+                return x;
+            else
+                return y;
+        }
+
+    }
+}
