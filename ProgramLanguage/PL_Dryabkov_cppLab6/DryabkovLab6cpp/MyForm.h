cbef630311c62778ad0f647206714c6c3bf12934
freekode1ko <freekode1ko@gmail.com>
2019-04-15 19:31:11 +0300

    Add remove selected element
    Some corrections

--- ProgramLanguage/PL_Dryabkov_cppLab6/DryabkovLab6cpp/MyForm.h
+++ ProgramLanguage/PL_Dryabkov_cppLab6/DryabkovLab6cpp/MyForm.h
@@ -41,6 +41,7 @@ namespace DryabkovLab6cpp {
 	private: System::Windows::Forms::DataGridView^  dataGridView2;
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
 
 	private:
 		/// <summary>
@@ -59,38 +60,51 @@ namespace DryabkovLab6cpp {
 			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
 			// 
+			this->dataGridView1->AllowUserToResizeColumns = false;
+			this->dataGridView1->AllowUserToResizeRows = false;
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::Control;
+			this->dataGridView1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->dataGridView1->ColumnHeadersBorderStyle = System::Windows::Forms::DataGridViewHeaderBorderStyle::None;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->ColumnHeadersVisible = false;
 			this->dataGridView1->Location = System::Drawing::Point(19, 12);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(708, 150);
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->ScrollBars = System::Windows::Forms::ScrollBars::Horizontal;
+			this->dataGridView1->Size = System::Drawing::Size(708, 69);
 			this->dataGridView1->TabIndex = 0;
 			// 
 			// dataGridView2
 			// 
+			this->dataGridView2->BackgroundColor = System::Drawing::SystemColors::Control;
+			this->dataGridView2->BorderStyle = System::Windows::Forms::BorderStyle::None;
 			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView2->Location = System::Drawing::Point(19, 207);
+			this->dataGridView2->ColumnHeadersVisible = false;
+			this->dataGridView2->Location = System::Drawing::Point(19, 112);
 			this->dataGridView2->Name = L"dataGridView2";
-			this->dataGridView2->Size = System::Drawing::Size(708, 150);
+			this->dataGridView2->RowHeadersVisible = false;
+			this->dataGridView2->ScrollBars = System::Windows::Forms::ScrollBars::Horizontal;
+			this->dataGridView2->Size = System::Drawing::Size(708, 69);
 			this->dataGridView2->TabIndex = 1;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(16, 179);
+			this->label1->Location = System::Drawing::Point(16, 84);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(0, 13);
 			this->label1->TabIndex = 2;
-			this->label1->Text = L"label1";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(283, 395);
+			this->button1->Location = System::Drawing::Point(284, 187);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(169, 23);
 			this->button1->TabIndex = 3;
@@ -98,11 +112,22 @@ namespace DryabkovLab6cpp {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(284, 217);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(169, 23);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"”далить элемент";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(739, 446);
+			this->ClientSize = System::Drawing::Size(739, 254);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->dataGridView2);
@@ -142,9 +167,17 @@ namespace DryabkovLab6cpp {
 			delete rezmasPtz;
 			delete masPtz;
 		}
-		catch (Exception^ e) {
-
-		}
+		catch (Exception^ e) {}
 	}
-	};
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
+	{
+		try
+		{
+			String^g = Interaction::InputBox("¬ведите количество элементов массива = ", "¬ведите значение", "", -1, -1);
+			int n = Convert::ToInt16(g);
+			dataGridView2->Columns->RemoveAt(n);//удал€ем колонку
+		}
+		catch(Exception^ e){}
+	}
+};
 }
