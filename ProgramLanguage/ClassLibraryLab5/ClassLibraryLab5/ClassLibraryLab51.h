3098b1794b463717e89d0b58a4e60e7d9e92e6c5
freekode1ko <freekode1ko@gmail.com>
2019-05-27 21:58:30 +0300

    Sepparate class
    Sepparate classlibrarryLab5 (was class2 -> class2+class3)
    (classlibrarryLab5.h+ classlibrarryLab5.cpp ->
    (classlibrarryLab51.h+ classlibrarryLab51.cpp )+
    (classlibrarryLab5.h+ classlibrarryLab5.cpp ))

---
+++ ProgramLanguage/ClassLibraryLab5/ClassLibraryLab5/ClassLibraryLab51.h
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "math.h"
+#pragma once
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+namespace ClassLibraryLab51
+{
+	public ref class Class3
+
+	{
+	public: static double Choose(double x, double b, double a);
+	public: static void Vivod(TextBox^ t, double x);
+	public: static double Vvod(TextBox^ t);
+	};
+}
