47b4bcd6816b0d10158a6ff77b087396be1137ec
freekode1ko <freekode1ko@gmail.com>
2019-06-04 17:45:37 +0300

    Lab 10
    All func calling from DLL Laba10Library

---
+++ ProgramLanguage/PL_Dryabkov_cppLab10/DryabkovLab6cpp/MyForm.h
@@ -0,0 +1,259 @@
+#pragma once
+
+namespace DryabkovLab6cpp {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace Laba10Library;
+	using namespace Microsoft::VisualBasic;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataGridView^  dataGridView1;
+	protected:
+	private: System::Windows::Forms::DataGridView^  dataGridView2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button1;
+
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::DataGridView^  dataGridView3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Button^  button2;
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView3 = (gcnew System::Windows::Forms::DataGridView());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView3))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToResizeColumns = false;
+			this->dataGridView1->AllowUserToResizeRows = false;
+			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::AllCells;
+			this->dataGridView1->AutoSizeRowsMode = System::Windows::Forms::DataGridViewAutoSizeRowsMode::AllCells;
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::Control;
+			this->dataGridView1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->dataGridView1->ColumnHeadersBorderStyle = System::Windows::Forms::DataGridViewHeaderBorderStyle::None;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->ColumnHeadersVisible = false;
+			this->dataGridView1->Location = System::Drawing::Point(12, 32);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->RowHeadersWidth = 21;
+			this->dataGridView1->ScrollBars = System::Windows::Forms::ScrollBars::Horizontal;
+			this->dataGridView1->Size = System::Drawing::Size(701, 173);
+			this->dataGridView1->TabIndex = 0;
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::AllCells;
+			this->dataGridView2->AutoSizeRowsMode = System::Windows::Forms::DataGridViewAutoSizeRowsMode::AllCells;
+			this->dataGridView2->BackgroundColor = System::Drawing::SystemColors::Control;
+			this->dataGridView2->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->ColumnHeadersVisible = false;
+			this->dataGridView2->Location = System::Drawing::Point(12, 249);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->RowHeadersVisible = false;
+			this->dataGridView2->ScrollBars = System::Windows::Forms::ScrollBars::Horizontal;
+			this->dataGridView2->Size = System::Drawing::Size(701, 173);
+			this->dataGridView2->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(812, 208);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 13);
+			this->label1->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(815, 152);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(169, 23);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Сгенерировать";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(13, 13);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(157, 13);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Исходная 2х мерная матрица";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(16, 233);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(78, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Рельтирущая ";
+			// 
+			// dataGridView3
+			// 
+			this->dataGridView3->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::AllCells;
+			this->dataGridView3->AutoSizeRowsMode = System::Windows::Forms::DataGridViewAutoSizeRowsMode::AllCells;
+			this->dataGridView3->BackgroundColor = System::Drawing::SystemColors::Control;
+			this->dataGridView3->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->dataGridView3->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView3->ColumnHeadersVisible = false;
+			this->dataGridView3->Location = System::Drawing::Point(12, 366);
+			this->dataGridView3->Name = L"dataGridView3";
+			this->dataGridView3->RowHeadersVisible = false;
+			this->dataGridView3->ScrollBars = System::Windows::Forms::ScrollBars::Horizontal;
+			this->dataGridView3->Size = System::Drawing::Size(701, 76);
+			this->dataGridView3->TabIndex = 7;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(16, 350);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(210, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Рельтирущая после удаления элемента";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(815, 182);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(169, 23);
+			this->button2->TabIndex = 9;
+			this->button2->Text = L"Удалить элемент";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1032, 551);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->dataGridView3);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->dataGridView2);
+			this->Controls->Add(this->dataGridView1);
+			this->Name = L"MyForm";
+			this->Text = L"Лабораторная работа №10";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView3))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+		//int arrayCount = 0;// номер группы массивов в базе данных
+		//int matrixCount = 0;// номер группы матриц в базе данных
+		int n = 0;// число строк
+		int m = 0;// число столбцов
+		int **mat;// исходная матрица
+		int max;// масимальный четный элемент
+		int k;// число элементов в результирующей матрице
+		int *rezmas;// результирующая матрица
+		//int *rezmas2;// отфильтрованная матрица 
+		//int **rezmat;// результирующая матрица
+
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) 
+	{
+		// ----------- Ввод количества строк и столбцов матрицы исходной матрицы -----------//
+		String^ g1 = Interaction::InputBox("Введите число строк матрицы:", "Число строк = ", "5", -1, -1);
+		String^ g2 = Interaction::InputBox("Введите число столбцов матрицы:", "Число стобцов = ", "5", -1, -1);
+		n = Convert::ToInt32(g1);
+		m = Convert::ToInt32(g2);
+		// ----------- Создание и вывод исходной матрицы -----------//
+		mat = new int*[n];
+		Lab10::input_mas2(mat, n, m);
+		Lab10::output_mas_int(mat, n, m, dataGridView1);
+		// ----------- Создание и вывод результирующего массива -----------//
+
+		max = Lab10::max_mas(mat, n, m);
+		Lab10::vivod_Label2(max, label1);
+		k = Lab10::set_count2(mat, n, m, max);
+		rezmas = new int[k];
+		Lab10::set_mas2(mat, rezmas, n, m, max);
+		Lab10::output_mas_int2(rezmas, k, dataGridView2);
+		Lab10::output_mas_int2(rezmas, k, dataGridView3);
+		// ----------- Создание и вывод отфильрованного массива -----------// 
+		//rezmas2 = Lab10::puzir(rezmas, k);
+		//Lab10::output_mas_int2(rezmas2, k, Grid3);
+		// ----------- Создание второй матрицы -----------//
+		//rezmat = new int*[n];
+		//Lab10::set_mas3(mat, rezmat, n, m, max);
+		//Lab10::output_mas_int(rezmat, n, m, Grid4);
+
+	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
+	{
+		// Выборка нужного элемента и его удаление
+		String^gg = Interaction::InputBox("Введите номер элемента для его удаления = ", " Введите значение ", "", -1, -1);
+		int kk = Convert::ToInt16(gg);
+		double *rezmasPtr2 = new double[100];
+		Lab10::ElDel(rezmasPtr2, kk, dataGridView3);
+		Lab10::out_ElDel(rezmasPtr2, dataGridView3);
+	}
+
+};
+}
