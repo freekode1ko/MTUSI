78a91ce445da6a3d25d2d204fcb5c92f08cffa68
freekode1ko <freekode1ko@gmail.com>
2019-06-11 00:20:16 +0300

    Dll for lab3
    for counting rating and outputs to textbox, chart

---
+++ ProgramLanguage/Class3Library/Class3Library/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+
+namespace Class3Library
+{
+    public class Class1
+    {
+        static public void Vivod(double x, TextBox t)
+        { t.Text = Convert.ToString(x); }
+
+        static public int rating(int n)
+        {
+            int rat = ((n + 1) / 3 + 2);
+            return rat;
+        }
+
+        static public void Viv(double x, string t, Chart c)
+        { c.Series[t].Points.AddY(x); }
+    }
+}
