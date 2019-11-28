4f6f4226a656b4f6d8f4aaaf29598fc6ad5a8fb8
freekode1ko <freekode1ko@gmail.com>
2019-11-28 19:47:54 +0300

    Add Lab 3

---
+++ OS/Lab3/Lab3/Lab3/Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lab3
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Task1 = new System.Windows.Forms.Button();
+            this.Task2 = new System.Windows.Forms.Button();
+            this.Task3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Task1
+            // 
+            this.Task1.Location = new System.Drawing.Point(12, 12);
+            this.Task1.Name = "Task1";
+            this.Task1.Size = new System.Drawing.Size(358, 87);
+            this.Task1.TabIndex = 0;
+            this.Task1.Text = "1";
+            this.Task1.UseVisualStyleBackColor = true;
+            this.Task1.Click += new System.EventHandler(this.TB_Task1_Click);
+            // 
+            // Task2
+            // 
+            this.Task2.Location = new System.Drawing.Point(12, 105);
+            this.Task2.Name = "Task2";
+            this.Task2.Size = new System.Drawing.Size(358, 87);
+            this.Task2.TabIndex = 0;
+            this.Task2.Text = "2";
+            this.Task2.UseVisualStyleBackColor = true;
+            this.Task2.Click += new System.EventHandler(this.TB_Task2_Click);
+            // 
+            // Task3
+            // 
+            this.Task3.Location = new System.Drawing.Point(12, 198);
+            this.Task3.Name = "Task3";
+            this.Task3.Size = new System.Drawing.Size(358, 87);
+            this.Task3.TabIndex = 0;
+            this.Task3.Text = "3";
+            this.Task3.UseVisualStyleBackColor = true;
+            this.Task3.Click += new System.EventHandler(this.TB_Task3_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 301);
+            this.Controls.Add(this.Task3);
+            this.Controls.Add(this.Task2);
+            this.Controls.Add(this.Task1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Task1;
+        private System.Windows.Forms.Button Task2;
+        private System.Windows.Forms.Button Task3;
+    }
+}
+
---
+++ OS/Lab3/Lab3/Lab3/Main.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Lab3
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+
+        class ClassCounter 
+        {
+            public delegate void MethodContainer();
+            public event MethodContainer onCount;
+            public void sb()
+            {
+                File.Delete(Directory.GetCurrentDirectory()+@"\Task3.txt");
+                for (int i = 0; ; i++)
+                {
+                    File.AppendAllText(Directory.GetCurrentDirectory() + @"\Task3.txt", "String\n");
+                    Thread.Sleep(50);
+                    if (i == 10)
+                    {
+                        onCount();
+                        i = 0;
+                    }
+                }
+            }
+        }
+        class Handler_I 
+        {
+            public void Message()
+            {
+                File.AppendAllText(Directory.GetCurrentDirectory() + @"\Task3.txt", "LOG FROM FIRST\n");
+            }
+        }
+        class Handler_T
+        {
+            public void Message()
+            {
+                File.AppendAllText(Directory.GetCurrentDirectory() + @"\Task3.txt", "LOG FROM SECOND\n");
+
+            }
+        }
+        static object locker = new object();
+
+        static void WriteInFile(string text)
+        {
+            lock (locker)
+            {
+                if (!File.Exists(Directory.GetCurrentDirectory() + @"\Task1.txt"))
+                    File.Create(Directory.GetCurrentDirectory()+ @"\Task1.txt");
+                else
+                    File.AppendAllText(Directory.GetCurrentDirectory()+ @"\Task1.txt", text);
+            }
+        }
+        static void WriteInFile1()
+        {
+            int i=0;
+            lock (locker)
+            {
+                for (i = 0; i < 5; i++)
+                {
+                    WriteInFile("First ");
+                }
+            }
+            i = 0;
+        }
+        static void WriteInFile2()
+        {
+            
+            int i=0;
+            lock (locker) 
+            { 
+                for (i=0;i<5;i++)
+                {
+                     WriteInFile("Second ");
+                }
+            }
+            i = 0;
+            
+        }
+        static void Terminate()
+        {
+            for (; ; Thread.Sleep(2000))
+            {
+                lock (locker)
+                {
+                    if (File.Exists(Directory.GetCurrentDirectory()+ @"\Task3.txt"))
+                    {
+                        File.Delete(Directory.GetCurrentDirectory() + @"\Task3.txt");
+                    }
+                }
+            }
+        }
+        bool existed;
+
+        private void TB_Task1_Click(object sender, EventArgs e)
+        {
+            Thread write1 = new Thread(WriteInFile1);
+            write1.IsBackground = true;
+            Thread write2 = new Thread(WriteInFile2);
+            write2.IsBackground = true;
+            Thread Read = new Thread(Terminate);
+            Read.IsBackground = true;
+            write1.Start();
+            write2.Start();
+            Read.Start();
+            MessageBox.Show("Close this meessage to proceed");
+            write1.Abort();
+            write2.Abort();
+            Read.Abort();
+        }
+
+        private void TB_Task2_Click(object sender, EventArgs e)
+        {
+            Mutex mutexObj = new Mutex(true, "guid", out existed);
+            if (existed)
+            {
+                MessageBox.Show("Application works in a single copy, Close this message to proceed");
+            }
+            else
+            {
+                MessageBox.Show("Application has already been launched, now it will be closed.");
+                Thread.Sleep(3000);
+                this.Close();
+            }
+        }
+
+        private void TB_Task3_Click(object sender, EventArgs e)
+        {
+            ClassCounter Counter = new ClassCounter();
+            Thread sob = new Thread(Counter.sb);
+            sob.Start();
+
+            bool Exists;
+            Mutex mO = new Mutex(true, "guidr", out Exists);
+            if (Exists)
+            {
+                Handler_I Handler1 = new Handler_I();
+                Counter.onCount += Handler1.Message;
+            }
+            else
+            {
+                Handler_T Handler2 = new Handler_T();
+                Counter.onCount += Handler2.Message;
+            }
+            MessageBox.Show("Close this meessage to proceed");
+            sob.Abort();
+        }
+    }
+}
