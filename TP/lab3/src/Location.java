1ad265c8b513dccf3562198db5ea89257d14eaa6
Freekode1ko <Freekode1ko@gmail.com>
2020-09-05 21:35:33 +0300

    add lab3

---
+++ TP/lab3/src/Location.java
@@ -0,0 +1,48 @@
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+    @Override
+    public boolean equals(Object obj)
+    {
+    	if (obj instanceof Location) 
+    	{
+    		Location other = (Location) obj;
+    		if (xCoord == other.xCoord && yCoord == other.yCoord) 
+    		{
+    			return true;
+    		}
+    	}
+		return false;
+    }
+    
+    public int hashcode()
+    {
+        int result = 17; // Some prime value
+        // Use another prime value to comnbine
+        result = 37 * result + xCoord;
+        result = 37 * result + yCoord;
+		return result;
+    }
+}
